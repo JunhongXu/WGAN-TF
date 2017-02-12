12ff41f653f4f9b0920eb34cc4b1c266a04aedf3
Junhong Xu <jxu7@bsu.edu>
2017-02-11 23:31:17 -0500

    first commit

---
+++ src/wgan.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+from tensorflow.contrib import layers as layers
+import numpy as np
+
+
+class WGAN(object):
+    """
+    Implementation of WGAN. Original paper can be found at "https://arxiv.org/abs/1701.07875"
+    """
+    def __init__(self):
+        pass
+
+    def discriminator(self, reuse):
+        pass
+
+    def generator(self, reuse):
+        pass
+
+    def build(self):
+        pass
